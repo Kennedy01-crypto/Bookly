bfc349e8220cc9a2f5b27a432b1f927f965360e8
Jim Kennedy <kennedyomata@gmail.com>
2025-08-08 00:16:06 +0300

    feat: added file path to env

--- src/config.py
+++ src/config.py
@@ -1,9 +1,15 @@
 # create a file to read env variables
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pathlib import Path
 
+# Go up one level from src to the project root
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 class Settings(BaseSettings):
     # define env variables
     DATABASE_URL: str
 
-    model_config = SettingsConfigDict(env_file= ".env", extra="ignore")
+    model_config = SettingsConfigDict(env_file= BASE_DIR / ".env", extra="ignore")
+
+
+Config = Settings() #type: ignore
