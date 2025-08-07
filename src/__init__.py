3d5232e40a11377b8f902f8b553b905a293d962e
Jim Kennedy <kennedyomata@gmail.com>
2025-08-08 00:12:43 +0300

    feat: added application lifespan

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
 from src.books.routes import book_router
+from contextlib import asynccontextmanager
+from src.db.main import init_db
+
+
+@asynccontextmanager
+async def life_span(app: FastAPI):
+
+    print(f"Server is starting up...")
+    await init_db()
+    yield #separate the two
+    print(f"Server is shutting down...")
 
 version= "v1"
 
 app = FastAPI(
+    lifespan=life_span,
     version=version,
     title="Bookly API",
     description="This is a simple API for managing books",
